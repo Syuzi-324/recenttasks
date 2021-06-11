25fcacff4530532f9a8db950d2f9529ef00ce81c
Syuzanna Simonyan <syuzanna.simonyan@wic.local>
2021-06-11 14:44:12 +0400

    first commit

--- hw8.js
+++ hw8.js
@@ -1,17 +1,44 @@
 
 
 // 8.Գտնել զանգվածի 2-րդ մեծագույն արժեքի ինդեքսը
-let arr=[105400,2500,47,6,3,2,843];
+// let arr=[105400,2500,47,6,3,2,843];
 
 
-let i = arr.indexOf(Math.max(...arr));
+// let i = arr.indexOf(Math.max(...arr));
 
-arr[i] = 0;
+// arr[i] = 0;
 
-console.log(arr)
+// console.log(arr)
 
-let k = arr.indexOf(Math.max(...arr));
+// let k = arr.indexOf(Math.max(...arr));
 
-console.log (k);
+// console.log (k);
 
 
+function secondmax(arr) {
+	let max1, max2;
+
+	if(arr[0] > arr[1]){
+		max1 = arr[0];
+		max2 = arr[1];
+	} else {	
+		max1 = arr[1];
+		max2 = arr[0];
+		}
+
+
+	for(let i = 2; i < arr.length; ++i) {
+
+		if(max1 < arr[i] && max2 < arr[i]) {
+			max2 = max1;
+			max1 = arr[i];
+			
+		} else if(max1 > arr[i] && max2 < arr[i]) {
+			max2 = arr[i];
+		} else continue; 
+	}
+	console.log(arr)
+	return arr.indexOf(max2);
+}
+
+console.log(secondmax([10,25,47,60,3,2,843]))
--- toString.js
+++
@@ -1,8 +0,0 @@
-let arr = [1, 4, 5, 40, 20];
-
-function toString(arr) {
-
-	return arr.join(',');
-}
-
-console.log(toString(arr));
